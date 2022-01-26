f39eb9266f611ead8cd1cb685ac6068049aee5e5
Alex <argeares97@gmail.com>
2022-01-26 11:04:15 +0200

    fix: assets icons

--- src/helpers/assets-list/aIcons/index.ts
+++ src/helpers/assets-list/aIcons/index.ts
@@ -1,42 +1,45 @@
-import asusd from './asusd.svg';
-import asnx from './asnx.svg';
-import atusd from './atusd.svg';
-import ausdt from './ausdt.svg';
-import ausdc from './ausdc.svg';
-import adai from './adai.svg';
-import abusd from './abusd.svg';
-import aeth from './aeth.svg';
-import arep from './arep.svg';
-import abat from './abat.svg';
-import alink from './alink.svg';
-import amkr from './amkr.svg';
-import amana from './amana.svg';
-import azrx from './azrx.svg';
-import aknc from './aknc.svg';
-import awbtc from './awbtc.svg';
-import alend from './alend.svg';
-import aenj from './aenj.svg';
-import aren from './aren.svg';
 import aaave from './aaave.svg';
-import ayfi from './ayfi.svg';
-import astkaave from './astkaave.svg';
-import agusd from './agusd.svg';
-import auni from './auni.svg';
-import aweth from './aweth.svg';
-import acrv from './acrv.svg';
-import abal from './abal.svg';
-import axsushi from './axsushi.svg';
-import amatic from './amatic.svg';
-import arenfil from './arenfil.svg';
 import aampl from './aampl.svg';
-import arai from './arai.svg';
-import apax from './apax.svg';
-import adpi from './adpi.svg';
 import aavax from './aavax.svg';
-import afrax from './afrax.svg';
-import afei from './afei.svg';
-import ausdp from './ausdp.svg';
+import abal from './abal.svg';
+import abat from './abat.svg';
+import abusd from './abusd.svg';
+import acrv from './acrv.svg';
+import adai from './adai.svg';
+import adpi from './adpi.svg';
+import aenj from './aenj.svg';
+import aeth from './aeth.svg';
 import aeurs from './aeurs.svg';
+import afei from './afei.svg';
+import afrax from './afrax.svg';
+import agusd from './agusd.svg';
+import aknc from './aknc.svg';
+import alend from './alend.svg';
+import alink from './alink.svg';
+import amana from './amana.svg';
+import amatic from './amatic.svg';
+import amkr from './amkr.svg';
+import apax from './apax.svg';
+import arai from './arai.svg';
+import aren from './aren.svg';
+import arenfil from './arenfil.svg';
+import arep from './arep.svg';
+import asnx from './asnx.svg';
+import astkaave from './astkaave.svg';
+import asusd from './asusd.svg';
+import atusd from './atusd.svg';
+import auni from './auni.svg';
+import ausdc from './ausdc.svg';
+import ausdp from './ausdp.svg';
+import ausdt from './ausdt.svg';
+import awbtc from './awbtc.svg';
+import aweth from './aweth.svg';
+import awmatic from './awmatic.svg';
+import axsushi from './axsushi.svg';
+import ayfi from './ayfi.svg';
+import azrx from './azrx.svg';
+import aftm from './aftm.svg';
+import aone from './aone.svg';
 
 interface Images {
   [name: string]: string;
@@ -82,4 +85,7 @@ export default {
   afei,
   ausdp,
   aeurs,
+  awmatic,
+  aftm,
+  aone,
 } as Images;
--- src/helpers/assets-list/icons/index.ts
+++ src/helpers/assets-list/icons/index.ts
@@ -1,48 +1,49 @@
 import aave from './aave.svg';
 import ampl from './ampl.svg';
+import avax from './avax.svg';
+import bal from './bal.svg';
 import bat from './bat.svg';
+import bpt from './bpt.svg';
 import busd from './busd.svg';
+import crv from './crv.svg';
 import dai from './dai.svg';
+import dpi from './dpi.svg';
+import enj from './enj.svg';
 import eth from './eth.svg';
+import eurs from './eurs.svg';
+import fei from './fei.svg';
+import frax from './frax.svg';
+import gusd from './gusd.svg';
 import knc from './knc.svg';
 import lend from './lend.svg';
 import link from './link.svg';
 import mana from './mana.svg';
+import matic from './matic.svg';
 import mkr from './mkr.svg';
+import pax from './pax.svg';
+import rai from './rai.svg';
+import ren from './ren.svg';
+import renfil from './renfil.svg';
 import rep from './rep.svg';
 import seth from './seth.svg';
 import snx from './snx.svg';
-import susd from './susd.svg';
-import tusd from './tusd.svg';
-import usdc from './usdc.svg';
-import usdt from './usdt.svg';
-import wbtc from './wbtc.svg';
-import zrx from './zrx.svg';
-import enj from './enj.svg';
-import ren from './ren.svg';
-import yfi from './yfi.svg';
-import uni from './uni.svg';
-import gusd from './gusd.svg';
-import weth from './weth.svg';
-import bpt from './bpt.svg';
 import stkaave from './stkAAVE.svg';
-import crv from './crv.svg';
-import bal from './bal.svg';
-import xsushi from './xsushi.svg';
-import matic from './matic.svg';
-import renfil from './renfil.svg';
-import rai from './rai.svg';
-import pax from './pax.svg';
-import dpi from './dpi.svg';
-import avax from './avax.svg';
-import wavax from './wavax.svg';
-import frax from './frax.svg';
-import fei from './fei.svg';
-import usdp from './usdp.svg';
+import susd from './susd.svg';
 import tribe from './tribe.svg';
-import eurs from './eurs.svg';
-import one from './one.svg';
+import tusd from './tusd.svg';
+import uni from './uni.svg';
+import usdc from './usdc.svg';
+import usdp from './usdp.svg';
+import usdt from './usdt.svg';
+import wavax from './wavax.svg';
+import wbtc from './wbtc.svg';
+import weth from './weth.svg';
+import wmatic from './wmatic.svg';
+import xsushi from './xsushi.svg';
+import yfi from './yfi.svg';
+import zrx from './zrx.svg';
 import ftm from './ftm.svg';
+import one from './one.svg';
 
 interface Images {
   [name: string]: string;
@@ -92,6 +93,7 @@ export default {
   usdp,
   tribe,
   eurs,
-  one,
+  wmatic,
   ftm,
+  one,
 } as Images;
